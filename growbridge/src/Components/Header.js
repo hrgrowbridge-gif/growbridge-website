bc1f81bee6113981b55f99739bdcfcc617da0ed4
unknown <keerthana.ramesh0811@gmail.com>
2026-03-20 20:51:31 +0530

    Header component added

---
+++ growbridge/src/Components/Header.js
@@ -0,0 +1,16 @@
+function Header() {
+  return (
+    <header>
+      <h1>GrowBridge</h1>
+      <nav>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
