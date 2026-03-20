d56a66c41ba1ec7b46e82e28176de117393e9b23
Venkatesh <hrgrowbridge@gmail.com>
2026-03-20 20:56:53 +0530

    footer added

---
+++ growbridge/src/Components/footer.js
@@ -0,0 +1,8 @@
+function footer() {
+    return (
+        <div className="footer">
+            <p>Copyright © 2024 GrowBridge. All rights reserved.</p>
+        </div>
+    );
+}
+export default footer;
--- growbridge/src/index.js
+++ growbridge/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Header from './Components/Header';
+import Footer from './Components/footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header />
     <App />
+    <Footer />
   </React.StrictMode>
 );
 
